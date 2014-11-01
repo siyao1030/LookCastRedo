b469beda9ba88c6a5cb2a1272acca8e49053b1e5
Siyao Xie <siyaoxie@siyaos-air-2.lan>
2014-11-01 11:29:22 -0700

    Initial Commit

---
+++ LookCast/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LookCast
+//
+//  Created by Siyao Xie on 11/1/14.
+//  Copyright (c) 2014 siyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
